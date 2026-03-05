6712f5222a65bc92d5cd8ce6c0ea78e72ef42b3c
RG <RG.com>
2026-03-05 20:55:36 -0300

    Feito a alteralçao de tema

--- js/index.js
+++ js/index.js
@@ -14,9 +14,30 @@ let tentativas = 0;
 
 
 
+window.onload = function () {
+    salvarTema();
+    buscarPalavras();
+    salvarPartida();
+};
 
-buscarPalavras()
-salvarPartida()
+
+
+
+
+function alterarTema() {
+    let tema = localStorage.getItem("tema") || 'light'
+    tema = tema == 'light' ? 'dark' : 'light';
+    document.body.dataset.theme = tema;
+    localStorage.setItem("tema", tema);
+}
+
+function salvarTema() {
+    const temaSalvo = localStorage.getItem("tema")
+    if(temaSalvo) {
+        document.body.dataset.theme = temaSalvo;
+    }
+}
+
 
 async function buscarPalavras() {
     try {
